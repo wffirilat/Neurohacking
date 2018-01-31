1e89518437dde9a023de7909630bb652af490e21
wffirilat <xwffirilat@gmail.com>
2018-01-31 13:57:30 -0600

    Worked on stuff. Don't question it

--- plugins/csv_collect.py
+++ plugins/csv_collect.py
@@ -1,5 +1,6 @@
 import csv
 import timeit
+import numpy as np
 import datetime
 
 import plugin_interface as plugintypes
@@ -7,6 +8,11 @@ import plugin_interface as plugintypes
 class PluginCSVCollect(plugintypes.IPluginExtended):
     def __init__(self, file_name="collect.csv", delim=",", verbose=False):
         super().__init__()
+        self.packetnum = -1
+        self.ticknum = -1
+        self.storelength = 1024
+        self.rawdata = np.zeros((8, self.storelength))
+        self.data = np.zeros((8, self.storelength))
         now = datetime.datetime.now()
         self.time_stamp = '%d-%d-%d_%d-%d-%d' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
         self.file_name = self.time_stamp
@@ -36,7 +42,20 @@ class PluginCSVCollect(plugintypes.IPluginExtended):
     def show_help(self):
         print("Optional argument: [filename] (default: collect.csv)")
 
+    @property
+    def current(self):
+        return self.data[:, self.ticknum % self.storelength]
+
     def __call__(self, sample):
+        self.ticknum += 1
+        if sample.id == 0:
+            self.packetnum += 1
+        self.rawdata[:, (sample.id + 256 * self.packetnum) % self.storelength] = sample.channel_data
+        self.data[:, (sample.id + 256 * self.packetnum) % self.storelength] = [v - avg for avg, v in zip(
+            [sum(self.rawdata[i, :]) / self.storelength for i in range(8)],
+            sample.channel_data
+        )]
+
         t = timeit.default_timer() - self.start_time
 
         # print(timeSinceStart|Sample Id)
@@ -48,10 +67,7 @@ class PluginCSVCollect(plugintypes.IPluginExtended):
         row += self.delim
         row += str(sample.id)
         row += self.delim
-        for i in sample.channel_data:
-            row += str(i)
-            row += self.delim
-        for i in sample.aux_data:
+        for i in self.current:
             row += str(i)
             row += self.delim
         # remove last comma
--- plugins/helpers.py
+++ plugins/helpers.py
@@ -7,13 +7,15 @@ Author: wffirilat
 
 import numpy as np
 
+from numpy.fft import fft, rfft
+
 import plugin_interface as plugintypes
 from open_bci_v3 import OpenBCISample
 
 class PluginHelpers(plugintypes.IPluginExtended):
     def __init__(self):
         self.packetnum = -1
-        self.ticknum = None
+        self.ticknum = -1
         self.storelength = 1024
         self.rawdata = np.zeros((8, self.storelength))
         self.data = np.zeros((8, self.storelength))
@@ -23,6 +25,7 @@ class PluginHelpers(plugintypes.IPluginExtended):
 
     # called with each new sample
     def __call__(self, sample: OpenBCISample):
+        self.ticknum += 1
         if sample.id == 0:
             self.packetnum += 1
         self.rawdata[:, (sample.id + 256 * self.packetnum) % self.storelength] = sample.channel_data
@@ -32,11 +35,17 @@ class PluginHelpers(plugintypes.IPluginExtended):
         )]
         self.print(sample)
 
+    def channelHistory(self, channel):
+        d = list(self.data[channel, self.ticknum % self.storelength:])
+        d.extend(self.data[channel, :self.ticknum%self.storelength])
+        return d
+
     def minmax(self, sample):
         print(min(self.data[3, :]), max(self.data[3, :]))
 
     def print(self, sample):
-        print(self.data[3,-1])
+        # print(self.data[3,-1])
+        print(rfft(self.channelHistory(3)))
 
     def thresholdDetect(self, sample):
         if self.data[3, (sample.id + 256 * self.packetnum) % self.storelength] > 1000:
