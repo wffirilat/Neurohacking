24ac91e59f727593081d0bd431bae29f1992fd50
TheKindlerofWildfires <Mjhock99@gmail.com>
2018-02-02 15:09:50 -0600

    Made startup less annoying

--- plugins/learn.py
+++ plugins/learn.py
@@ -27,48 +27,6 @@ from open_bci_v3 import OpenBCISample
 class PluginLearn(plugintypes.IPluginExtended):
     def __init__(self):
         #Does not give a warning when run here
-        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-        names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-        dataset = pandas.read_csv(url, names=names)
-        # dataset.plot(kind = 'box', subplots=True, layout=(2,2), sharex=False)
-        # dataset.hist()
-        # scatter_matrix(dataset)
-        array = dataset.values
-        X = array[:, 0:4]
-        Y = array[:, 4]
-        validation_size = 0.20
-        seed = 7
-        scoring = 'accuracy'
-        X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
-                                                                                        random_state=seed)
-        models = []
-        models.append(('LR', LogisticRegression()))
-        models.append(('LDR', LinearDiscriminantAnalysis()))
-        models.append(('KNN', KNeighborsClassifier()))
-        models.append(('CART', DecisionTreeClassifier()))
-        models.append(('NB', GaussianNB()))
-        models.append(('SVM', SVC()))
-        results = []
-        names = []
-        for name, model in models:
-            kfold = model_selection.KFold(n_splits=10, random_state=seed)
-            cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold)
-            results.append(cv_results)
-            names.append(name)
-            msg = "%s:%f(%f)" % (name, cv_results.mean(), cv_results.std())
-            print(msg)
-        fig = plt.figure()
-        fig.suptitle('Alg comp')
-        ax = fig.add_subplot(111)
-        plt.boxplot(results)
-        ax.set_xticklabels(names)
-        # plt.show()
-        knn = KNeighborsClassifier()
-        knn.fit(X_train, Y_train)
-        predictions = knn.predict(X_validation)
-        print(accuracy_score(Y_validation, predictions))
-        print(confusion_matrix(Y_validation, predictions))
-        print(classification_report(Y_validation, predictions))
         self.packetnum = -1
         self.ticknum = None
         self.storelength = 1024
