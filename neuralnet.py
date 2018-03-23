ef7af109da5e20dc5236512c5fcc72cf4e31b84f
wffirilat <xwffirilat@gmail.com>
2018-03-23 17:26:08 -0500

    Work from 2018-3-23

--- neuralnet.py
+++ neuralnet.py
@@ -5,85 +5,87 @@ File: neuralnet.py
 Author: kindler
 """
 import random
-import pandas
+
+import numpy
 from sklearn import model_selection
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-import numpy
+from sklearn.tree import DecisionTreeClassifier
+
+class Model:
+    LR = LogisticRegression
+    LDA = LinearDiscriminantAnalysis
+    KNN = KNeighborsClassifier
+    CART = DecisionTreeClassifier
+    NB = GaussianNB
+    SVM = SVC
+
 class NeuralNet:
     """ Takes in a data set and trains with it"""
+
     def __init__(self, validation_size, model, scoring):
         """This is the part where you tell it what kind of training you want to do"""
-        self.validation_size = validation_size #0.2 = 20%
-        self.dataset = numpy.empty
-        if model == "LR":
-            self.model = LogisticRegression()
-        elif model == "LDA":
-            self.model = LinearDiscriminantAnalysis()
-        elif model == "KNN":
-            self.model = KNeighborsClassifier()
-        elif model == "CART":
-            self.model = DecisionTreeClassifier()
-        elif model == "NB":
-            self.model =GaussianNB()
-        elif model == "SVM":
-            self.model =SVC()
-        self.scoring = scoring #Possible options = only scoring I think
+        self.validation_size = validation_size  # 0.2 = 20%
+        # self.dataset = numpy.empty
+        self.model = model()
+        self.scoring = scoring  # Possible options = only scoring I think
 
     def data(self, subset):
         """This part should add an item to the data to train from"""
         """Currently all this does is add data for training"""
         self.dataset = subset
 
-    def train(self):
+    def train(self, data):
         """Does all the training all at once"""
-        array = self.dataset
-        X = array[:, 0:-1]
-        Y = array[:, -1]
+        X = data[:, 0:-1]
+        Y = data[:, -1]
         '''
         This seperates the number from the actual data I think, number needs to be changed to meet the amount of data it is given(8?)
         For example List number E1, E2, E3,E4, E5, E6, E7, E8 Right/Left
         I think that I could probably write some code that used the length of the element in the list to do this automatically,
         but keeper will do that better
         '''
-        seed = random.randint(1,9)
+        seed = random.randint(1, 9)
         # This splits the data into training and validation
-        self.X_train, self.X_validation, self.Y_train, self.Y_validation = model_selection.train_test_split(X, Y, test_size=self.validation_size, random_state=seed)
+        self.X_train, self.X_validation, self.Y_train, self.Y_validation = (
+            model_selection.train_test_split(X, Y, test_size=self.validation_size, random_state=seed)
+        )
         # Idk what n_splits does, but i think this is where it decides where to start training on the data
         kfold = model_selection.KFold(n_splits=10, random_state=seed)
-        #And this is where it actually does the training
+        # And this is where it actually does the training
         cv_results = model_selection.cross_val_score(self.model, self.X_train, self.Y_train, cv=kfold, scoring=self.scoring)
         msg = "%f (%f)" % (cv_results.mean(), cv_results.std())
         print(msg)
+
     def quality(self):
         self.model.fit(self.X_train, self.Y_train)
         predictions = self.model.predict(self.X_validation)
         print(accuracy_score(self.Y_validation, predictions))
         print(confusion_matrix(self.Y_validation, predictions))
         print(classification_report(self.Y_validation, predictions))
+
     def get(self, pdata):
         """So the theory behind this function is that it makes a prediction about what the data you gave it was"""
-        self.model.fit(self.X_train, self.Y_train) # What does this do? Who knows! When I take it away here there are no problems, but in quality it causes crashes when absent
+        self.model.fit(self.X_train, self.Y_train)  # What does this do? Who knows! When I take it away here there are no problems, but in quality it causes crashes when absent
         predictions = self.model.predict(pdata)
         print(predictions)
         return (predictions)
 
     def validate(self, action, pdata):
         """So this takes the data you put it and sees if it made the correct guess"""
-        return self.get(pdata)==action
+        return self.get(pdata) == action
 
 '''def main():
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
     dataset = pandas.read_csv(url, names=names)
-    nn = NeuralNet(0.2, 'LR','accuracy')
+    nn = NeuralNet(0.2, Model.LR,'accuracy')
     print (type(dataset.values))
     nn.add(dataset.values)
     nn.train()
--- plugins/training.py
+++ plugins/training.py
@@ -11,10 +11,9 @@ from numpy.fft import fft, rfft
 import plugin_interface as plugintypes
 
 from enum import Enum
-from neuralnet import NeuralNet
+from neuralnet import NeuralNet, Model
 import random
 import time
-
 
 class States(Enum):
     INIT = 'INIT'
@@ -37,12 +36,9 @@ class PluginTraining(plugintypes.IPluginExtended):
         self.rawdata = np.zeros((8, self.storelength))
         self.data = np.zeros((8, self.storelength))
         self.state = States.INIT
-        self.nn = NeuralNet(0.2, 'LR','accuracy')
+        self.nn = NeuralNet(0.2, Model.LR, 'accuracy')
         self.actiondata = {action: [] for action in actions}
         self.recorded = np.zeros((1000,))
-        self.adder = 0
-
-
 
     def activate(self):
         print("training activated")
@@ -62,11 +58,11 @@ class PluginTraining(plugintypes.IPluginExtended):
         if self.state is States.INIT and self.ticknum >= self.storelength:
             print('Get ready for **INSTRUCTIONS**')
             self.state = States.ACQUIRE
-        if self.state is States.ACQUIRE:
-            if self.adder>10:
+        if self.state in (States.ACQUIRE, States.GATHERED, States.TRAIN):
+            if self.packetnum > 10:
                 '''When it has ten data points'''
-                self.nn.data(self.recorded)
-                self.nn.train()
+                # self.nn.data(self.recorded)
+                self.nn.train(self.recorded)
                 self.nn.quality()
                 self.state = States.INTERACT
             else:
@@ -75,7 +71,6 @@ class PluginTraining(plugintypes.IPluginExtended):
         if self.state is States.INTERACT:
             self.interact(self.data)
 
-
     def process(self, data):
         '''        ...  # TODO IDK what any of this was supposed to do
         self.actiondata[self.nextinst] = ...
@@ -83,32 +78,35 @@ class PluginTraining(plugintypes.IPluginExtended):
         print(self.getRandInstruction())
         self.nextinst = ... # ^that
         '''
-        #Do this step only once
-        dt = time.time()-self.starttime()
+        print("In process: State is ", self.state)
+        # Do this step only once
+        dt = time.time() - self.starttime
         if self.state == States.ACQUIRE:
-            instruction = actions[random.randint(0,1)]
-            print("Move your hand %s")%(instruction)
-            self.state == States.TRAIN
+            instruction = actions[random.randint(0, 1)]
+            print("Move your hand %s" % (instruction))
+            self.state = States.TRAIN
         if self.state == States.TRAIN:
-            if dt>1 and dt<2:
-                temp = data #Yeah I get that this is bad but what can you do?
+            if dt > 1 and dt < 2:  # TODO: refactor out time. use packets instead.
+                temp = data  # Yeah I get that this is bad but what can you do?
                 self.state = States.GATHERED
 
         if self.state == States.GATHERED:
-            temp = fft(temp)
+            # noinspection PyUnboundLocalVariable
+            temp = fft(temp, 60)
             print(temp)
-            self.recorded = np.append(self.recorded, temp)
-            self.adder += 1
+            self.recorded = np.hstack((self.recorded, temp))
+            print("self.recorded.shape =", self.recorded.shape)
             self.state = States.ACQUIRE
-        #Fft the data and store it with the instruction to recorded
-
+            # Fft the data and store it with the instruction to recorded
 
     def train(self):
         """Trains incrementally (we think - not right now)"""
         ...  # TODO
 
-    def validate(self): ...  # TODO
-        # getData()
+    def validate(self):
+        ...  # TODO
+
+    # getData()
 
 
 
@@ -123,4 +121,3 @@ class PluginTraining(plugintypes.IPluginExtended):
     def interact(self, sample):
         sample = fft(sample)
         print(self.nn.get(sample))
-
